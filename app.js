ad0fc7e760afd3d30b5ae88cbe63e4f80e50881a
bobsfriend12 <calebsethherring+github@gmail.com>
2021-04-17 20:29:11 -0500

    finished all audio. halfway through making the random one work.

--- app.js
+++ app.js
@@ -4,44 +4,53 @@ const audios = $("audio").get();
 
 //loop through each of the buttons
 for (let i = 0; i < ids.length; i++) {
-	const btn = $(".fx-btn")[i];
 	const id = $(".fx-btn")[i].id;
-	const selId = $(`#${id}`);
 
 	//set IsPlaying to false for all buttons
 	sessionStorage.setItem(`${id}IsPlaying`, "false");
-
-	const state = sessionStorage.getItem(`${id}IsPlaying`);
-
-	//create event listeners for each button
-	btn.addEventListener("click", () => {
-		const audio = document.getElementById(`${id}Audio`);
-		const state = sessionStorage.getItem(`${id}IsPlaying`);
-
-		//if its not playing, play it
-		if (state == "false") {
-			selId.addClass("playing");
-			audio.play();
-			sessionStorage.setItem(`${id}IsPlaying`, "true");
-		}
-		//if its playing pause it and reset it back to the beginning
-		else if (state == "true") {
-			//remove the playing class
-			selId.removeClass("playing");
-			audio.pause();
-			audio.currentTime = 0;
-			sessionStorage.setItem(`${id}IsPlaying`, "false");
-		}
-	});
 }
+
+$(".board-container").on("click", e => {
+	let target = e.target.id;
+
+	if (target === "random") {
+		const ran = Math.floor(Math.random() * 27);
+		let audTarget = ids[ran].id;
+	} else {
+		let audTarget = target;
+	}
+
+	const audio = document.getElementById(`${audTarget}Audio`);
+	const state = sessionStorage.getItem(`${target}IsPlaying`);
+	const selId = $(`#${target}`);
+
+	if (target === "faintScreams") {
+		audio.volume = 0.115;
+	}
+
+	//if its not playing, play it
+	if (state == "false") {
+		selId.addClass("playing");
+		audio.play();
+		sessionStorage.setItem(`${target}IsPlaying`, "true");
+	}
+	//if its playing pause it and reset it back to the beginning
+	else if (state == "true") {
+		//remove the playing class
+		selId.removeClass("playing");
+		audio.pause();
+		audio.currentTime = 0;
+		sessionStorage.setItem(`${target}IsPlaying`, "false");
+	}
+});
 
 for (let i = 0; i < audios.length; i++) {
 	const aud = audios[i];
-	const currAud = $("audio")[i].id;
 
 	aud.addEventListener("ended", () => {
 		const aud = audios[i];
-		const id = $(".fx-btn")[i].id;
+		const audId = aud.id;
+		const id = audId.slice(0, -5);
 		const selId = $(`#${id}`);
 
 		//remove the playing class
