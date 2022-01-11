fdea09f1c747c89f40651b711965bb10c73b080b
bobsfriend12 <calebsethherring+github@gmail.com>
2022-01-11 15:08:14 +0000

    commity

--- index.js
+++ index.js
@@ -9,16 +9,32 @@ const app = express();
 
 const NodeCouchDb = require("node-couchdb");
 
-const couch = new NodeCouchDb();
+const couch = new NodeCouchDb({
+	host: 'ampere.calebsservers.ga',
+    protocol: 'http',
+    port: 5984,
+	auth: {
+		user: "backend",
+		password: "BoardBackend2012"
+	}
+});
+const dbName = "boards";
+const viewURL = "_design/main/_view/main";
 
 app.use("/file", express.static(path.join(__dirname, "static/audio")));
 
 //Get object
-app.get("/boards", (req, res) => {});
+app.get("/boards", (req, res) => {
+	console.log("finding")
+	couch.get(dbName, viewURL).then(({data, headers, status}) => {
+		console.log(data);
+		res.send("success");
+	});
+});
 
 //Update object
 app.post("/boards", (req, res) => {});
 
-app.listen(port, () => {
+app.listen(3000, () => {
 	console.log(`Listening on port ${port}. http://localhost:${port}`);
 });
