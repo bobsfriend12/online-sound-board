c48ec20f2de0ab0632ad46a0b96babb5068cf592
bobsfriend12 <50430839+bobsfriend12@users.noreply.github.com>
2023-01-14 21:58:31 -0600

    CSS improvment (#8)
    * sidebar fixed and loading screen
    
    * fixed somethings
    
    * better main page
    
    * add no board handling
    
    * fix delete board problem and get rid of warnings
    
    * get rid of console logs
    
    * sidebar fix for real this time
    
    * new colors
    
    * add looping
    
    * fix modal layering
    
    * fix merge conflict
    
    Co-authored-by: bobsfriend12 <calebsethherring@gmail.com>

--- src/components/core/SideLayout/SideLayout.jsx
+++ src/components/core/SideLayout/SideLayout.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 import "./SideLayout.css";
+
+import DatabaseContext from "../../../contexts/DatabaseContext";
 
 import Sidebar from "../Sidebar/Sidebar";
 import Dashboard from "../../pages/Dashboard/Dashboard";
 import EditBoard from "../../pages/EditBoard/EditBoard";
+import Modal from "../../blocks/Modal/Modal";
 
 function SideLayout(currBoard, page) {
-	return (
-		<div className="side_layout">
-			<div className="side_layout__sidebar">
-				<Sidebar />
-			</div>
-			<div className="side_layout__main">
-				{page === "dashboard" ? (
-					<Dashboard board={currBoard} />
-				) : (
-					<EditBoard board={currBoard} />
-				)}
-			</div>
-		</div>
-	);
+  const [show, setShow] = useState(false);
+  const context = useContext(DatabaseContext);
+  const { dbResults, editBoard } = context;
+  const boards = dbResults.boards;
+  const { boardId } = useParams();
+
+  let navigate = useNavigate();
+
+  function onSave(newBoardObj) {
+    setShow(false);
+    editBoard(newBoardObj);
+    navigate(`/dashboard/${newBoardObj.id}`);
+  }
+
+  return (
+    <div className="side_layout">
+      <div className="side_layout__sidebar">
+        <Sidebar func={setShow} />
+      </div>
+      <div className="side_layout__main">
+        {page === "dashboard" ? (
+          <>
+            <Dashboard board={currBoard} />
+            <Modal
+              type="newBoard"
+              show={show}
+              onClose={() => setShow(false)}
+              onSave={onSave}
+              boards={boards}
+            />
+          </>
+        ) : (
+          <EditBoard board={currBoard} />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default SideLayout;
--- src/components/core/Sidebar/Sidebar.jsx
+++ src/components/core/Sidebar/Sidebar.jsx
@@ -8,88 +8,87 @@ import Modal from "../../blocks/Modal/Modal";
 
 import DatabaseContext from "../../../contexts/DatabaseContext";
 
-function Sidebar() {
-	const [show, setShow] = useState(false);
-	const context = useContext(DatabaseContext);
-	const { dbResults, editBoard } = context;
-	const boards = dbResults.boards;
-	const { boardId } = useParams();
+function Sidebar({ func }) {
+  // const [show, setShow] = useState(false);
+  const context = useContext(DatabaseContext);
+  const { dbResults, editBoard } = context;
+  const boards = dbResults.boards;
+  const { boardId } = useParams();
+  const setShow = func;
 
-	let navigate = useNavigate();
+  let navigate = useNavigate();
 
-	function onSave(newBoardObj) {
-		setShow(false);
-		editBoard(newBoardObj);
-		navigate(`/dashboard/${newBoardObj.id}`);
-	}
+  // function onSave(newBoardObj) {
+  //   setShow(false);
+  //   editBoard(newBoardObj);
+  //   navigate(`/dashboard/${newBoardObj.id}`);
+  // }
 
-	if (boards.length === 0) {
-		return (
-			<div className="sidebar">
-				<div className="sidebar__top">
-					<h2 className="sidebar__title">Boards</h2>
-					<ul className="sidebar__list">
-						<li className="sidebar__item">There are no Boards</li>
-					</ul>
-				</div>
-				<div className="sidebar__bottom">
-					<Btn
-						content="Add Board"
-						extraClasses="sidebar__btn"
-						onClick={() => setShow(true)}
-					/>
-				</div>
-				<Modal
+  if (boards.length === 0) {
+    return (
+      <div className="sidebar">
+        <div className="sidebar__top">
+          <h2 className="sidebar__title">Boards</h2>
+          <ul className="sidebar__list">
+            <li className="sidebar__item">There are no Boards</li>
+          </ul>
+        </div>
+        <div className="sidebar__bottom">
+          <Btn
+            content="Add Board"
+            extraClasses="sidebar__btn"
+            onClick={() => setShow(true)}
+          />
+        </div>
+        {/* <Modal
 					type="newBoard"
 					show={show}
 					onClose={() => setShow(false)}
 					onSave={onSave}
 					boards={boards}
-				/>
-			</div>
-		);
-	}
+				/> */}
+      </div>
+    );
+  }
 
-	return (
-		<div className="sidebar">
-			<div className="sidebar__top">
-				<h2 className="sidebar__title">Boards</h2>
-				<ul className="sidebar__list">
-					{boards
-						.slice(0)
-						.reverse()
-						.map((board, index) => (
-							<li
-								key={index}
-								className={
-									boardId === board.id
-										? "sidebar__item--selected"
-										: "sidebar__item"
-								}
-							>
-								<Link to={`/dashboard/${board.id}`}>
-									{board.title}
-								</Link>
-							</li>
-						))}
-				</ul>
-			</div>
-			<div className="sidebar__bottom">
-				<Btn
-					content="Add Board"
-					extraClasses="sidebar__btn"
-					onClick={() => setShow(true)}
-				/>
-			</div>
-			<Modal
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h2 className="sidebar__title">Boards</h2>
+        <ul className="sidebar__list">
+          {boards
+            .slice(0)
+            .reverse()
+            .map((board, index) => (
+              <li
+                key={index}
+                className={
+                  boardId === board.id
+                    ? "sidebar__item--selected"
+                    : "sidebar__item"
+                }
+              >
+                <Link to={`/dashboard/${board.id}`}>{board.title}</Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="sidebar__bottom">
+        <Btn
+          content="Add Board"
+          extraClasses="sidebar__btn"
+          onClick={() => setShow(true)}
+        />
+      </div>
+      {/* <Modal
 				type="newBoard"
 				show={show}
 				onClose={() => setShow(false)}
 				onSave={onSave}
 				boards={boards}
-			/>
-		</div>
-	);
+			/> */}
+    </div>
+  );
 }
 
 export default Sidebar;
