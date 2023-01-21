83175cece351187d3eb8fa3ef3d352fa0c9773bf
bobsfriend12 <50430839+bobsfriend12@users.noreply.github.com>
2023-01-21 17:16:34 -0600

    Fun new features (#11)
    * sidebar fixed and loading screen
    
    * fixed somethings
    
    * better main page
    
    * add no board handling
    
    * fix delete board problem and get rid of warnings
    
    * get rid of console logs
    
    * sidebar fix for real this time
    
    * new colors
    
    * add looping
    
    * fix modal layering
    
    * fix merge conflict
    
    * add error page
    
    * multiple at a time, space pauses
    
    * remove console.logs
    
    Co-authored-by: bobsfriend12 <calebsethherring@gmail.com>

--- src/components/core/App/App.jsx
+++ src/components/core/App/App.jsx
@@ -3,156 +3,148 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import BaseLayout from "../BaseLayout/BaseLayout";
 import Loading from "../Loading/Loading";
+import Error from "../Error/Error";
 import DatabaseContext from "../../../contexts/DatabaseContext";
 
 let dbResults = {};
 
 function App() {
-	const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-	const editBoard = (newBoard) => {
-		let newIndex;
-		const newBoardIndex = dbResults.boards.findIndex(
-			(i) => i.id === newBoard.id
-		);
-		if (newBoardIndex === -1) {
-			newIndex = dbResults.boards.length;
-		} else {
-			newIndex = newBoardIndex;
-		}
-		dbResults.boards[newIndex] = newBoard;
+  const editBoard = (newBoard) => {
+    let newIndex;
+    const newBoardIndex = dbResults.boards.findIndex(
+      (i) => i.id === newBoard.id
+    );
+    if (newBoardIndex === -1) {
+      newIndex = dbResults.boards.length;
+    } else {
+      newIndex = newBoardIndex;
+    }
+    dbResults.boards[newIndex] = newBoard;
 
-		//For new boards
-		if (newIndex === dbResults.numOfBoards) {
-			dbResults.numOfBoards++;
+    //For new boards
+    if (newIndex === dbResults.numOfBoards) {
+      dbResults.numOfBoards++;
 
-			fetch(`${process.env.REACT_APP_API_URL}/new/board`, {
-				method: "POST",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify(newBoard)
-			})
-				.then(() => {
-					// reloadDb();
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		} else {
-			fetch(`${process.env.REACT_APP_API_URL}/update/board`, {
-				method: "POST",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify(newBoard)
-			})
-				.then(() => {})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-	};
+      fetch(`${process.env.REACT_APP_API_URL}/new/board`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newBoard),
+      })
+        .then(() => {
+          // reloadDb();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      fetch(`${process.env.REACT_APP_API_URL}/update/board`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newBoard),
+      })
+        .then(() => {})
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
 
-	const deleteBoard = (board) => {
-		let boards = dbResults.boards;
+  const deleteBoard = (board) => {
+    let boards = dbResults.boards;
 
-		boards.splice(boards.indexOf(board), 1);
+    boards.splice(boards.indexOf(board), 1);
 
-		fetch(`${process.env.REACT_APP_API_URL}/board`, {
-			method: "DELETE",
-			mode: "cors",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(board)
-		})
-			.then(() => {
-				window.location.reload();
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+    fetch(`${process.env.REACT_APP_API_URL}/board`, {
+      method: "DELETE",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(board),
+    })
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	useEffect(() => {
-		console.log(process.env.REACT_APP_API_URL);
-		fetch(`${process.env.REACT_APP_API_URL}/boards`)
-			.then((res) => {
-				res.json().then((json) => {
-					dbResults = json;
-					setLoading(false);
-				});
-			})
-			.catch((err) => {
-				//TODO: Handle error
-				// const ers = JSON.stringify(err);
-				setLoading(false);
-			});
-	}, []);
-	const boards = dbResults.boards;
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_URL);
+    fetch(`${process.env.REACT_APP_API_URL}/boards`)
+      .then((res) => {
+        res.json().then((json) => {
+          dbResults = json;
+          setLoading(false);
+        });
+      })
+      .catch((err) => {
+        //TODO: Handle error
+        console.log(err);
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+  const boards = dbResults.boards;
 
-	if (loading) {
-		return <Loading />;
-	}
+  if (loading) {
+    return <Loading />;
+  }
 
-	let boardToRedirect;
+  if (error) {
+    return <Error />;
+  }
 
-	if (boards.length > 0) {
-		boardToRedirect = boards[dbResults.numOfBoards - 1].id;
-	} else {
-		boardToRedirect = "no-boards";
-	}
+  let boardToRedirect;
 
-	return (
-		<DatabaseContext.Provider value={{ dbResults, editBoard, deleteBoard }}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="*" element={<BaseLayout page="notFound" />} />
-					<Route
-						exact
-						path="/"
-						element={<BaseLayout page="main" />}
-					/>
-					<Route
-						path="/dashboard"
-						element={
-							<Navigate to={boardToRedirect} replace={true} />
-						}
-					/>
-					<Route
-						path="/dashboard/:boardId"
-						element={<BaseLayout page="dashboard" />}
-					/>
-					<Route
-						path="/edit"
-						element={
-							<Navigate
-								to={"/dashboard/" + boardToRedirect}
-								replace={true}
-							/>
-						}
-					/>
-					<Route
-						path="/edit/:boardId"
-						element={<BaseLayout page="edit" />}
-					/>
-					<Route
-						path="/edit/:boardId/:soundId"
-						element={<BaseLayout page="edit" />}
-					/>
-					<Route
-						path="/board/:boardId"
-						element={<BaseLayout page="board" />}
-					/>
-					<Route path="/loading" element={<Loading />} />
-				</Routes>
-			</BrowserRouter>
-		</DatabaseContext.Provider>
-	);
+  if (boards.length > 0) {
+    boardToRedirect = boards[dbResults.numOfBoards - 1].id;
+  } else {
+    boardToRedirect = "no-boards";
+  }
+
+  return (
+    <DatabaseContext.Provider value={{ dbResults, editBoard, deleteBoard }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<BaseLayout page="notFound" />} />
+          <Route exact path="/" element={<BaseLayout page="main" />} />
+          <Route
+            path="/dashboard"
+            element={<Navigate to={boardToRedirect} replace={true} />}
+          />
+          <Route
+            path="/dashboard/:boardId"
+            element={<BaseLayout page="dashboard" />}
+          />
+          <Route
+            path="/edit"
+            element={
+              <Navigate to={"/dashboard/" + boardToRedirect} replace={true} />
+            }
+          />
+          <Route path="/edit/:boardId" element={<BaseLayout page="edit" />} />
+          <Route
+            path="/edit/:boardId/:soundId"
+            element={<BaseLayout page="edit" />}
+          />
+          <Route path="/board/:boardId" element={<BaseLayout page="board" />} />
+          <Route path="/loading" element={<Loading />} />
+        </Routes>
+      </BrowserRouter>
+    </DatabaseContext.Provider>
+  );
 }
 
 export default App;
